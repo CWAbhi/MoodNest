d7873899be172fa214a0e307c2c1418abebbd0c2
Abhijeet Dey <dey20405@gmail.com>
2025-12-03 15:19:00 +0530

    Fixed cookies error

--- backend/src/utils/jwt.js
+++ backend/src/utils/jwt.js
@@ -15,8 +15,8 @@ export const verifyToken = (token) => {
 export const setTokenCookie = (res, token) => {
   res.cookie('token', token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    secure: true,
+    sameSite: 'none',
     maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
   })
 }
